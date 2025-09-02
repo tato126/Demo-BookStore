8ad864bb2515f112649b475f795af844363eb705
tato126 <heechan.log@gmail.com>
2025-09-02 22:40:09 +0900

    JPA 설정 추가

--- src/main/java/org/example/demoBookStore/product/entity/ProductRepository.java
+++ src/main/java/org/example/demoBookStore/product/entity/ProductRepository.java
@@ -1,4 +1,19 @@
 package org.example.demoBookStore.product.entity;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Book, Long> {
+
+    // 제목으로 찾기
+    List<Book> findByBookNameContaining(String bookName);
+
+    // 작가명으로 찾기
+    List<Book> findByAuthor(String author);
+
+    // Isbn으로 찾기
+    Optional<Book> findByIsbn(Long isbn);
+
 }
